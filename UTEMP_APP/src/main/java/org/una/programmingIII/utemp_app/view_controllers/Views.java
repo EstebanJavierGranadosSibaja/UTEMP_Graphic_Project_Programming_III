66a66d78bc1f6aef72a3c4117aaefab7a9054357
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2024-10-30 15:04:21 -0600

    Create Views.java

---
+++ UTEMP_APP/src/main/java/org/una/programmingIII/utemp_app/view_controllers/Views.java
@@ -0,0 +1,13 @@
+package org.una.programmingIII.utemp_app.view_controllers;
+
+public class Views {
+    public static final String MAIN_VIEW = "/views/Login.fxml";
+    public static final String LOGIN = "/views/Login.fxml";
+    public static final String MENU = "/views/Menu.fxml";
+    public static final String CURSE = "/views/Register.fxml";//TODO
+    public static final String USERS = "/views/About.fxml";
+
+    private Views() {
+        throw new UnsupportedOperationException("No se puede instanciar esta clase.");
+    }
+}
