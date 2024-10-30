fb29a56f3086068921f3d936df761528d2ec4bea
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2024-10-30 15:04:26 -0600

    Update UTEMP_Application.java

--- UTEMP_APP/src/main/java/org/una/programmingIII/utemp_app/UTEMP_Application.java
+++ UTEMP_APP/src/main/java/org/una/programmingIII/utemp_app/UTEMP_Application.java
@@ -3,6 +3,7 @@ package org.una.programmingIII.utemp_app;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import org.una.programmingIII.utemp_app.manager.ScreenManager;
+import org.una.programmingIII.utemp_app.view_controllers.Views;
 
 public class UTEMP_Application extends Application {
 
@@ -13,8 +14,7 @@ public class UTEMP_Application extends Application {
     @Override
     public void start(Stage primaryStage) {
         ScreenManager screenManager = new ScreenManager(primaryStage);
-//        screenManager.loadScreen("/fxml/MapManager.fxml", "Initial views");
-        screenManager.loadScreen("/views/Login.fxml", "Initial views");
+        screenManager.loadScreen(Views.MAIN_VIEW, "Initial views");
 
         Singleton singleton = Singleton.getInstance();
         singleton.setScreenManager(screenManager);
