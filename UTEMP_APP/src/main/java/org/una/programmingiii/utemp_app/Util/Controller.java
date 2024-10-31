243f89c8755c85299376a3037a984b8d1f49d4da
Noemi-Murillo <noemi.murillo.godinez@est.una.ac.cr>
2024-10-31 16:04:00 -0600

    feat (MainViewController)

---
+++ UTEMP_APP/src/main/java/org/una/programmingiii/utemp_app/Util/Controller.java
@@ -0,0 +1,45 @@
+package org.una.programmingiii.utemp_app.Util;
+
+import javafx.scene.control.Control;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+public abstract class Controller {
+
+    Stage stage;
+    private String accion;
+    private String nombreVista;
+
+    public String getNombreVista() {
+        return nombreVista;
+    }
+
+    public void setNombreVista(String nombreVista) {
+        this.nombreVista = nombreVista;
+    }
+
+    public String getAccion() {
+        return accion;
+    }
+
+    public void setAccion(String accion) {
+        this.accion = accion;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void sendTabEvent(KeyEvent event) {
+        event.consume();
+        KeyEvent keyEvent = new KeyEvent(KeyEvent.KEY_PRESSED, null, null, KeyCode.TAB, false, false, false, false);
+        ((Control) event.getSource()).fireEvent(keyEvent);
+    }
+
+    public abstract void initialize();
+}
--- UTEMP_APP/src/main/java/org/una/programmingiii/utemp_app/ViewControllers/MainBackgroundViewController.java
+++ UTEMP_APP/src/main/java/org/una/programmingiii/utemp_app/ViewControllers/MainBackgroundViewController.java
@@ -1,4 +1,43 @@
 package org.una.programmingiii.utemp_app.ViewControllers;
 
-public class MainBackgroundViewController {
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import org.una.programmingiii.utemp_app.Util.Controller;
+
+public class MainBackgroundViewController extends Controller implements Initializable {
+    @FXML
+    private ImageView imvLogo;
+
+    @FXML
+    private MFXButton btnUsers;
+
+    @FXML
+    private MFXButton btnCourses;
+
+    @FXML
+    private MFXButton btnMyCourses;
+
+    @FXML
+    private ImageView imvNotifications;
+
+    @FXML
+    private ImageView imvUser;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        initialize();
+    }
+
+    @Override
+    public void initialize() {
+
+    }
 }
