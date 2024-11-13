2673a1c37971a01fc7b439942510ea111ab03157
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2024-11-12 21:14:14 -0600

    Update FileMetadatumDTO.java

--- UTEMP_APP/src/main/java/org/una/programmingIII/utemp_app/dtos/FileMetadatumDTO.java
+++ UTEMP_APP/src/main/java/org/una/programmingIII/utemp_app/dtos/FileMetadatumDTO.java
@@ -17,6 +17,7 @@ import java.time.LocalDateTime;
 @Builder
 public class FileMetadatumDTO {
 
+    // datos sobre el archivo guardado
     private Long id;
 
     @Builder.Default
@@ -24,18 +25,7 @@ public class FileMetadatumDTO {
     private SubmissionDTO submission = new SubmissionDTO();
 
     @Builder.Default
-//    @JsonBackReference("file-user")
     private UserDTO student = new UserDTO();
-
-    @NotNull(message = "File name must not be null")
-    @Size(max = 255, message = "File name must be at most 255 characters long")
-    private String fileName;
-
-    @NotNull(message = "File size must not be null")
-    private Long fileSize;
-
-    @Size(max = 100, message = "File type must be at most 100 characters long")
-    private String fileType;
 
     @Size(max = 500, message = "Storage path must be at most 500 characters long")
     private String storagePath;
@@ -45,6 +35,17 @@ public class FileMetadatumDTO {
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS")
     private LocalDateTime lastUpdate;
+
+    // para donwload o upload el archivo
+    @NotNull(message = "File name must not be null")
+    @Size(max = 255, message = "File name must be at most 255 characters long")
+    private String fileName;
+
+    @NotNull(message = "File size must not be null")
+    private Long fileSize;
+
+    @Size(max = 100, message = "File type must be at most 100 characters long")
+    private String fileType;
 
     // Nuevos campos para manejo de fragmentos
     private byte[] fileChunk; // Fragmento del archivo
